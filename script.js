21730fba9a387378f02b4f997a48382e40f58dc0
brandonlambrecht <brandonjlambrecht@gmail.com>
2023-09-20 16:47:44 -0500

    created setTimeBlocks functions,updated HTML structure

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // TODO: Add code to display the current date in the header of the page.
 var currentDay = $('#current-day');
+var timeBlocks = $('.time-block');
+var saveButton = $('.saveBtn')
+
+
 
 function displayTime() {
 
@@ -17,6 +21,7 @@ setInterval(displayTime, 1000);
 
 // }
 // TODO: Add a listener for click events on the save button. This code should
+
 // use the id in the containing time-block as a key to save the user input in
 // local storage. HINT: What does `this` reference in the click listener
 // function? How can DOM traversal be used to get the "hour-x" id of the
@@ -24,6 +29,37 @@ setInterval(displayTime, 1000);
 // useful when saving the description in local storage?
 //
 // TODO: Add code to apply the past, present, or future class to each time
+function setTimeBlocks() {
+  var currentTime = dayjs().hour()
+  timeBlocks.each(function () {
+    console.log(this)
+    var timeBlockTime = parseInt($(this).attr("id").split("-")[1])
+    console.log(timeBlockTime, currentTime)
+    if (currentTime > timeBlockTime) {
+      $(this).addClass('past')
+
+    } else if (currentTime < timeBlockTime) {
+      $(this).addClass('future')
+    } else {
+      $(this).addClass('present')
+    }
+    var savedNote = localStorage.getItem($(this).attr("id"))
+    console.log(savedNote)
+
+    // var displayNote = localStorage.getItem($(this).attr("id"))
+
+    //Select this timeBlock ($(this).attr("id") children and select text area. Set value equal to savedNote
+  })
+}
+setTimeBlocks()
+
+saveButton.on('click', function () {
+  var value = $(this).siblings()[1].value
+  var key = $(this).parent().attr('id')
+
+  localStorage.setItem(key, value)
+})
+
 // block by comparing the id to the current hour. HINTS: How can the id
 // attribute of each time-block be used to conditionally add or remove the
 // past, present, and future classes? How can Day.js be used to get the
